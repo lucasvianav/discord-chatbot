092378c822a4cdeaa156f06d66c2cd912479e24d
Lucas Viana <oviana@outlook.com>
2021-01-02 23:15:36 -0300

    .secrets.json --> .env

--- config.py
+++ config.py
@@ -1,15 +1,22 @@
-import json
+from dotenv import load_dotenv
+from os import getenv
 
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+load_dotenv()
+
 # Gets tokens and keys
-with open(".secrets.json", "r") as f: data = json.load(f)
-DISCORD_TOKEN = data["discord-token"]
-SPREADSHEET_KEY = data["spreadsheet-key"]
+DISCORD_TOKEN = getenv('DISCORD_TOKEN')
+SPREADSHEET_KEY = getenv('SPREADSHEET_KEY')
 
 # Use creds to create a client to interact with the Google Drive API
-scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    'https://www.googleapis.com/auth/spreadsheets',
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
 creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
 client = gspread.authorize(creds)
 
