61fd77d1ffea4c3ccf97dfc0d82a8cae621c74b6
Arthur Grillo <arthur.grillo@usp.br>
2021-06-13 23:29:10 -0300

    :man: para :man_judge:

--- cogs/Counters.py
+++ cogs/Counters.py
@@ -46,7 +46,7 @@ class Counters(commands.Cog):
 
         response = await ctx.send(f'*Leonardo* ||Baptistella||, foi preso `{self.counters["bap"]}` vezes.')
 
-        await reactToResponse(self.bot, response, ['👨'])    
+        await reactToResponse(self.bot, response, ['\:man_judge:'])    
 
 def setup(bot):
     bot.add_cog(Counters(bot))
