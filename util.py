aa406f03b94d18dd6616f7080b502c29f3aacc48
Lucas Viana <oviana@outlook.com>
2021-07-03 20:29:05 -0300

    uploading image (MOLETOM.jpeg)

--- util.py
+++ util.py
@@ -131,7 +131,10 @@ def writeCogs(cogSheet: list, commands: list):
 
                 cog.write("        if images:\n")
                 cog.write(f"            response = await ctx.{'send' if not reply else 'reply'}(content=txt, files=[ discord.File(img) for img in images ], tts={tts})\n")
-                cog.write("            for img in images: os.remove(img)\n\n")
+                # COMPREM MOLETONS
+                # cog.write("            for img in images: os.remove(img)\n\n")
+                cog.write("            for img in images:")
+                cog.write("                if not img.endswith('MOLETOM.jpeg'): os.remove(img)\n\n")
 
                 cog.write(f"        else: response = await ctx.{'send' if not reply else 'reply'}(content=txt, tts={tts})\n\n")
 
